168dff80f5b3d50e533784aab3a6eb5b3950a7c8
pipixia <pipixia@pipi.com>
2017-02-25 15:19:39 +0800

    实现登录和退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -1,8 +1,37 @@
+// 1 进行登录的判断
+// 根据 cookie 中有没有 PHPSESSID
+// 如果没有，就说明用户没有登录，此时，应该跳到：登录页面！
 
-	NProgress.start();
+// 只要是通过登录页面登录的，此时，服务器就会自动给我们设置好一个 PHPSESSID
+// 但是，如果没有登录，此时，说明没有这个id，那么，就让用户跳到登录页面进行登录！
 
-	NProgress.done();
+if(!$.cookie('PHPSESSID') && location.pathname !== '/login') {
+	location.href = '/login';
+}
 
-	$('.navs ul').prev('a').on('click', function () {
-		$(this).next().slideToggle();
+// 根据用户登录的时候， 设置好的cookie 获取到用户的用户名和头像
+// 进行设置！
+var userinfo = JSON.parse( $.cookie('userinfo') );
+var $profile = $('.profile');
+// 查找当前元素的所有后代元素
+$profile.find('img').attr('src', userinfo.tc_avatar)
+$profile.find('h4').text(userinfo.tc_name);
+
+
+// 退出功能！
+$('#logout').on('click', function() {
+	// alert(123213);
+	if( !confirm('您确定要退出吗？') ) {
+		return;
+	}
+
+	$.ajax({
+		url: '/api/logout',
+		type: 'post',
+		success: function( data ) {
+			if( data.code === '200' ) {
+				location.href = '/login';
+			}
+		}
 	});
+});
